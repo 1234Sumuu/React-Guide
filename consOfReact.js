f2be5d0fd15824662107b36bb04fa8ee1a666548
1234Sumuu <41938215+1234Sumuu@users.noreply.github.com>
2023-08-14 10:51:09 +0600

    consOfReact.js

---
+++ consOfReact.js
@@ -0,0 +1,12 @@
+// JSX example within JavaScript
+import React from 'react';
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  );
+}
